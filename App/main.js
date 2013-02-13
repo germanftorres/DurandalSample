bc8c3aaed13c7c070566f62ce0d287efc98c566e
germanftorres <germanftorres@outlook.com>
2013-02-13 17:22:24 +0100

    Sample

---
+++ App/main.js
@@ -0,0 +1,22 @@
+﻿requirejs.config({
+    paths: {
+        'text': 'durandal/amd/text'
+    }
+});
+
+define(function(require) {
+    var app = require('durandal/app'),
+        system = require('durandal/system'),
+        viewLocator = require('durandal/viewLocator');
+
+    system.debug(true);
+
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        app.adaptToDevice();
+        app.setRoot('viewmodels/shell');
+    });
+});
---
+++ App/viewmodels/editor.js
@@ -0,0 +1,57 @@
+﻿define(function (require) {
+
+    var app = require("durandal/app"),
+        viewModel = require("durandal/viewModel");
+
+
+    function TabItem(caption, iconClass, template, selected) {
+        var self = this;
+        this.caption = caption;
+        this.iconClass = iconClass;
+        this.template = template;
+        this.selected = selected;
+        this.isActive = ko.computed(function () {
+            return self == selected();
+        });
+    }
+
+    function Editor() {
+        this.paquete = ko.observable();        
+        this.selectedTab = ko.observable();
+        this.tabs = ko.observableArray([
+            new TabItem("General", "icon-file", "_general", this.selectedTab),
+            new TabItem("Opciones aereo", "icon-plane", "_opcionesaereo", this.selectedTab)
+        ]);
+        this.selectedTab(this.tabs()[0]);
+    };
+
+    Editor.prototype = {
+        activate: function (data) {
+            var that = this;
+
+            // mock some data
+            // var paquete = http.get("api/...");            
+            var paquete = {
+                id: "paquetes/00001",
+                description: ko.observable("Estancia en Bahamas"),
+                items: ko.observableArray([
+                  {
+                      id: ko.observable(1),
+                      description: ko.observable("First Item")
+                  },
+                  {
+                      id: ko.observable(1),
+                      description: ko.observable("Second Item")
+                  },
+                  {
+                      id: ko.observable(1),
+                      description: ko.observable("Third")
+                  }
+                ])
+            };
+
+            that.paquete(paquete);
+        }
+    };
+    return Editor;
+});
